8dd3e60d676903d77beab0b9073934c61aee4645
Nitish Singh <nitishsingh925@gmail.com>
2024-09-07 16:24:56 +0530

    add migrations

---
+++ src/database/data-source.ts
@@ -0,0 +1,23 @@
+import { DataSource } from 'typeorm';
+
+process.loadEnvFile();
+export const dataSource = new DataSource({
+  type: 'mysql',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  entities: ['dist/**/*.entity.js'],
+  migrations: ['dist/database/migrations/*{.ts,.js}'],
+  synchronize: false,
+});
+
+dataSource
+  .initialize()
+  .then(() => {
+    console.log('Data Source has been initialized!');
+  })
+  .catch((err) => {
+    console.error('Error during Data Source initialization:', err);
+  });
---
+++ src/database/migrations/1725705331570-InitialMigration.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class InitialMigration1725705331570 implements MigrationInterface {
+    name = 'InitialMigration1725705331570'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE \`user\` (\`id\` int NOT NULL AUTO_INCREMENT, \`username\` varchar(255) NOT NULL, \`email\` varchar(255) NOT NULL, \`name\` varchar(255) NOT NULL, \`password\` varchar(255) NOT NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE \`user\``);
+    }
+
+}
